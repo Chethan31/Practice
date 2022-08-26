506a813afff29e2066eeae8d7d7dc02beb332f27
Chethan Gowda <chethanrgowda38@gmail.com>
2022-08-26 13:07:36 +0530

    Add

---
+++ MathProblemOfTwoNumbers/Data/Validate.cs
@@ -0,0 +1,40 @@
+﻿using SumOfTwoNumbers.Data;
+using SumOfTwoNumbers.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathProblemOfTwoNumbers.Data
+{
+    public class Validate
+    {
+        public static void valid(DataOfTwoNumbers data)
+        {
+            IMaths maths = null;
+            IDataRepo file = new FileRepo();
+            IDataRepo database = new DBRepo();
+            double res;
+            switch (data.Operator)
+            {
+                case '+':
+                    maths = new Addition();
+                    data.result = maths.result(data.FirstNumber, data.SecondNumber);
+                    Console.WriteLine("The Sum of two Numbers:" + data.result);
+                    file.Save(data);
+                    database.Save(data);
+                    break;
+                case '-':
+                    maths = new Subtraction();
+                    data.result = maths.result(data.FirstNumber, data.SecondNumber);
+                    Console.WriteLine("The Sub of two Numbers:" + data.result);
+                    file.Save(data);
+                    database.Save(data);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ MathProblemOfTwoNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using MathProblemOfTwoNumbers.Data;
+using SumOfTwoNumbers.Data;
+using SumOfTwoNumbers.Entities;
+
+namespace SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadData();
+        }
+        static void ReadData()
+        {
+            DataOfTwoNumbers data = new DataOfTwoNumbers();
+            Console.WriteLine("Enter First Number:");
+            data.FirstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            data.SecondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the operation:");
+            data.Operator = char.Parse(Console.ReadLine());
+            Validate.valid(data);
+        }   
+    }
+}
